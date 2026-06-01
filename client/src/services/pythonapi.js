80a13484c57e3464d71a9a19c43e5964817f7c59
Venkata Tarun Panangipalli <tarun.panangipalli@motivitylabs.com>
2026-06-01 17:27:48 +0530

    Backend frontend connection established

--- client/src/services/pythonapi.js
+++ client/src/services/pythonapi.js
@@ -1,6 +1,6 @@
 const API_BASE =
-  "http://localhost:8000";
-  //https://python-backend-rmd.onrender.com;
+  //"http://localhost:8000";
+  "https://python-backend-rmd.onrender.com";
 
 export const generateProjection =
   async (
